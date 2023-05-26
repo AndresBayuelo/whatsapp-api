5869dafc85a229c59a96156956d2d68d12177125
Andres Bayuelo <andres.bayuelo@hotmail.com>
2023-05-26 00:37:52 +0000

    Configuraciones adicionales en NestJS
    - Se realizaron los correspondientes cambios para el manejo de
    variables de entorno en los respectivos archivos .env.
    - Se cargaron los módulos y se estableció la respectiva configuración
    para hacer uso de la documentación por medio de Swagger.
    - Se instaló el módulo de axios para hacer uso de las peticiones HTTP.
    - Se ejecutó la función enableCors para permitir el acceso a la API
    desde cualquier origen.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { environments } from './environments';
+import config from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({}),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
